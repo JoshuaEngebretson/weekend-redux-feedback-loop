d7ae53a1df5c4b0663176987bcf538813d0943ee
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-05-05 18:59:04 -0500

    Built out POST route within feedback.router.js
    - This should add the req.body into the "feedback" table

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -4,13 +4,43 @@ const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
     pool.query('SELECT * from "feedback";')
-    .then((result) => {
-        res.send(result.rows);
+    .then((dbRes) => {
+        res.send(dbRes.rows);
     })
-    .catch((error) => {
-        console.log('Error GET /feedback', error)
+    .catch((dbErr) => {
+        console.log('Error GET /feedback', dbErr)
         res.sendStatus(500);
     });
+})
+
+router.post('/', (req, res) => {
+    const feedback = req.body;
+
+    sqlText = `
+        INSERT INTO feedback
+        ("feeling", "understanding", "support", "comments")
+        VALUES
+        ($1, $2, $3, $4);
+    `;
+
+    sqlValues = [
+        feedback.feeling,
+        feedback.understanding,
+        feedback.support,
+        feedback.comments
+    ];
+
+    pool.query(sqlText, sqlValues)
+        .then((dbRes) => {
+            // Send "Created" status
+            req.sendStatus(201);
+        })
+        .catch((dbErr) => {
+            // On Error, log the error
+            console.log('Error with POST /feedback request:', dbErr);
+            // Send "Internal Server Error" status to client
+            res.sendStatus(500)
+        })
 })
 
 module.exports = router;

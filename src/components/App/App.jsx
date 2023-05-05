cbd600f30c8a09abe51f69fd2484170635354dd2
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-05-04 20:13:04 -0500

    Built GET path for /feedback
    - When getFeedback is called, the redux state is set with the feedbackArray

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getFeedback();
+  }, []);
+
+  const getFeedback = () => {
+    axios({
+      type: 'GET',
+      url: '/feedback'
+    }).then ((response) => {
+      const feedbackArray = response.data;
+      dispatch({
+        type: 'SET_FEEDBACK_REDUCER',
+        payload: feedbackArray
+      })
+    })
+  }
+
+  const getPizzas = () => {
+    axios({
+      method: 'GET',
+      url: '/api/pizza'
+    }).then ((response) => {
+      const pizzaArray = response.data;
+      dispatch({
+        type: 'SET_PIZZA_REDUCER',
+        payload: pizzaArray
+      })
+    })
+  }
 
   return (
     <div className='App'>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
+// Redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+const feedbackReducer = (state = [], action) => {
+    if (action.type = 'SET_FEEDBACK_REDUCER') {
+        feedbackArray = action.payload;
+        return feedbackArray;
+    }
+    return state
+}
+
+const theStore = createStore(
+    combineReducers({
+        feedbackReducer
+    }),
+    applyMiddleware(
+        logger
+    )
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={theStore}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );

a030cf398b8f8ac38e13e0243ff82a675d68a2c4
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-05-06 11:32:45 -0500

    Started working with contentEditable field inside of CommentsPage instead of text area
    - This is being used to instead have an auto-resize feature.
    - Working on how to render the comments on the ReviewFeedbackPage to reflect multiple lines correctly.

--- src/components/FeedbackComponents/CommentsPage/CommentsPage.jsx
+++ src/components/FeedbackComponents/CommentsPage/CommentsPage.jsx
@@ -19,18 +19,29 @@ function CommentsPage() {
   return (
     <>
       <h1>Any comments you want to leave?</h1>
-      <textarea 
+      <div className="above-input">
+        <label>Comments</label>
+      </div>
+    <p>
+      <span 
+        className='textarea'
+        role='textbox'
+        contentEditable
+        placeholder="Enter Optional Comments Here..."
+        value={feedbackComments}
+        onInput={event => setFeedbackComments(event.target.innerHTML)}>
+      </span>
+    </p>
+      {/* <textarea 
         rows='4'
-        cols='60'
+        cols='30'
         placeholder='Optional'
         value={feedbackComments}
         onChange={event => setFeedbackComments(event.target.value)}
       >
-        {/* This is where the user will type their comments. */}
-      </textarea>
-      <div>
+        This is where the user will type their comments.
+      </textarea> */}
         <button className='next-btn' onClick={dispatchSendToReview}>Next</button>
-      </div>
     </>
   )
 }

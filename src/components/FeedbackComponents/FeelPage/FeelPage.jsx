2c8be7ff9cd00028d7398c4f5be1acfb0df6bb1f
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-05-06 09:19:25 -0500

    Added styling to input box on FeelPage
    - Only has border on the bottom, removed outline when field is selected.
    - When user attempts to progress to the next page and the field is not filled in correctly
    	- Input validation checks if number is entered AND if the number is between 1-5
    	- If these are not met, the border-bottom becomes red and the placeholder is colored red.

--- src/components/FeedbackComponents/FeelPage/FeelPage.jsx
+++ src/components/FeedbackComponents/FeelPage/FeelPage.jsx
@@ -7,21 +7,35 @@ function FeelPage() {
 
   const history = useHistory();
   const dispatch = useDispatch();
-  const [feelingsInput, setFeelingsInput] = useState('')
+  const [feelingsInput, setFeelingsInput] = useState('');
+  const [requiredInput, setRequiredInput] = useState(false);
 
   const checkRequiredSendToUnderstand = () => {
-    if (feelingsInput !== '') {
+    if (feelingsInput !== '' && feelingsInput >= 1 && feelingsInput <= 5) {
       dispatch({
         type: 'SET_FEELINGS_RATING',
         payload: feelingsInput
       })
       history.push('/understanding')
+      setRequiredInput(false);
     }
     else {
       Swal.fire({
         icon: 'warning',
-        text: 'Please fill out the required field'
+        html: `
+          <p>Please complete the required field.</p>
+          <p>With a number between 1 and 5</p>`
       })
+      setRequiredInput(true);
+    }
+  }
+
+  const required = () => {
+    if (requiredInput) {
+      return 'required'
+    }
+    else {
+      return 'bottom-border'
     }
   }
 
@@ -30,6 +44,7 @@ function FeelPage() {
       <h1>How are you feeling today?</h1>
       <p>On a scale from 1-5</p>
       <input 
+        className={required()}
         type='number'
         placeholder='Required'
         value={feelingsInput}
